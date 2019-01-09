2400045d92ad01ed84a88cf42f6d43e93e31c847
kaichiro <kcfg23@gmail.com>
2019-01-09 08:10:10 -0200

    Add component Form

---
+++ react-prototipo/src/components/CarrinhoDeComprasComponent.js
@@ -0,0 +1,10 @@
+function CarrinhoDeComprasComponent() {
+  return (
+    // eslint-disable-next-line react/react-in-jsx-scope
+    <div>
+      <del>kaichiro</del>
+    </div>
+  );
+}
+
+export default CarrinhoDeComprasComponent;
